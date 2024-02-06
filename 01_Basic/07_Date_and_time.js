aa610579a64d76602b74cd5afd2fa1bfd092a3a3
kundan singh <139676878+kundansinghdev@users.noreply.github.com>
2024-02-06 17:48:45 +0000

    test

---
+++ 01_Basic/07_Date_and_time.js
@@ -0,0 +1,10 @@
+// Dates
+// let mydate=new Date();
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toLocaleDateString());
+// console.log(typeof(mydate));
+
+// let myCreateDate=new Date(2023,0,23)
+// console.log(myCreateDate.toString());
+
--- 01_Basic/test.js
+++ 01_Basic/test.js
@@ -1,1 +1,59 @@
-console.log("Manav here")
+// {
+//     let a=1;
+//     let b=2;
+//     console.log(a);
+//     console.log(b);
+// }
+// console.log(a);
+// console.log(b);
+// console.log(a); 
+// var a=5
+
+// console.log(name);
+// name is used to gobal varibles
+// var name='kundan'
+// console.log(typeof kundan)
+
+
+// const user1={
+//     name:'kundan',
+//     age:25,
+//     address:{
+//         city:'Mumbai',
+//         state:'bihar'
+//     },
+// }
+
+// const user2=user1
+// user2.name='ramesh'
+// user2.address.city='pune'
+// console.log(user1); 
+// console.log(user2);
+
+// const user1 = {
+//     name: 'kundan',
+//     age: 25,
+//     address: {
+//         city: 'Mumbai',
+//         state: 'bihar'
+//     }
+// };
+
+// const user2 = JSON.parse(JSON.stringify(user1)); // Create a deep copy of user1
+// user2.name = 'ramesh';
+// user2.address.city = 'pune';
+
+// console.log(user1); 
+// console.log(user2);
+// console.log([1,2,3]==[1,2,3]);
+// console.log(({name: 'sonu'}) === ({name: 'sonu'})); // false
+
+// const numnber=[10,20,30,40,50]
+// const div=numnber.map(n=>n/2);
+
+// console.log(div)
+
+
+const numnber=[0,10,20,30,40,50]
+const divfilter=numnber.filter(n=>n<20);
+console.log(divfilter)
