c575791b75dc7321c87dea88a8dd99f3f0caa8f3
kundan singh <139676878+kundansinghdev@users.noreply.github.com>
2024-02-06 11:58:39 +0000

    number and math

---
+++ 01_Basic/05_strings.js
@@ -0,0 +1,24 @@
+let firstName="kundan";
+let secondName="singh";
+// console.log(firstName+secondName);
+// console.log(`My first Name is ${firstName} and last name is ${secondName}`);
+
+const gameName= new String('kundan          ');
+// console.log(gameName.length);
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toLowerCase());
+// console.log(gameName.charAt(5));
+// const newstring=gameName.substring(0,4);
+// console.log(newstring);
+// const newstring=gameName.slice(-12,8);
+// console.log(newstring);
+// console.log(gameName.trim());
+
+
+// const url="kundansingh#4%39xydb.com"
+// console.log(url.replace('4%','*'));
+// console.log(url.includes('sundr'));
+const url="kundan#singh#manav#kumar#xyz";
+console.log(url.split('#'))
---
+++ 01_Basic/06_number_math.js
@@ -0,0 +1,33 @@
+const score=400
+
+// const balance=new Number(100)
+// // console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber=23.534;
+// console.log(otherNumber.toPrecision(3));
+
+
+const hundread=100000;
+// console.log(hundread.toLocaleString('en-IN'));
+
+
+
+
+//+++++++++ Math ++++++
+
+console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(5.5));
+// console.log(Math.ceil(3.2))
+// console.log(Math.floor(3.2))
+// console.log(Math.min(12,31,32,42,13))
+
+// console.log(Math.max(23,12,32,13,42,13,4))
+
+// console.log(Math.random());
+// console.log(Math.random()*10+1);
+
+const min=10;
+const max=20;
+console.log(Math.floor(Math.random()*(max-min+1))+min)
