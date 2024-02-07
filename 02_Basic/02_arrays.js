93782cd4c04654652ae8402c6956e2821c83dcd2
kundan singh <139676878+kundansinghdev@users.noreply.github.com>
2024-02-07 08:33:17 +0000

    array

---
+++ 02_Basic/02_arrays.js
@@ -0,0 +1,27 @@
+const boyname=["Ram","Mohan","kundan","manav"]
+const girlname=["saline","komal", "manshi"]
+
+// console.log(boyname/girlname
+// boyname.push(girlname);
+// console.log(boyname);
+// const newdats= boyname.concat(girlname)
+// console.log(newdats);
+// const newdata=[...boyname,...girlname]
+// console.log(newdata);
+// const otherarray=[1,2,4,5,[4,5],3,5,[2,5,3,[34,23,23]]];
+// const useusalearray=otherarray.flat(Infinity);
+// console.log(useusalearray);
+
+
+// console.log(Array.isArray('kundan'))
+// console.log(Array.from("kundan"));
+ console.log(Array.from({name:"kundan"}));// interesting
+
+
+// let score1=100;
+// let score2=200;
+// let score3=300;
+// console.log(Array.of(score1,score2,score3));
+
+
+console.log(Array.from("cimage"));
