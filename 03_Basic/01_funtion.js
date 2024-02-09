5df632800d9b9b849dd38a71868af5ed3eee7d69
kundan singh <139676878+kundansinghdev@users.noreply.github.com>
2024-02-09 18:00:23 +0000

    object

---
+++ 03_Basic/01_funtion.js
@@ -0,0 +1,24 @@
+function addtwoNumber(number1,number2){
+    // console.log(number1+number2);
+    let result=number1+number2;
+    return result
+}
+
+
+
+// let store=addtwoNumber(1,3)
+// console.log(store);
+
+
+function calculatecartPrice(val1,val2,...num1){
+    return num1
+} 
+
+//console.log(calculatecartPrice(200,400,500,2000));
+const user={
+    username:"kundan",
+    price:99
+}
+function handleobject(anyobject){
+    console.log();
+}
