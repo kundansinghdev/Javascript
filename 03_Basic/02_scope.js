0ed1c3217021310ed4b8b1f68a32a5945a84311f
kundan singh <139676878+kundansinghdev@users.noreply.github.com>
2024-02-10 01:54:19 +0000

    commit

---
+++ 03_Basic/02_scope.js
@@ -0,0 +1,20 @@
+var c=300
+let a=300
+if(true){
+    let a=10
+    const b=20
+    // console.log("Inner :",a);   
+}
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+function adcone(value){
+    return value+1;
+}
+adcone(5)
+
+const addtwo=function(num){
+    return num+2
+}
+addtwo(5)
